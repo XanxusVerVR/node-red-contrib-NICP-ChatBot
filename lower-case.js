11afb4b87184cb0df0008671e500f162e7c68fbb
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2019-02-04 17:32:21 +0800

    為set、change...四個操作都加上可帶入context物件的下拉選項

--- lower-case.js
+++ lower-case.js
@@ -1,12 +1,15 @@
+const fs = require("fs");
 module.exports = function (RED) {
     function LowerCaseNode(config) {
         RED.nodes.createNode(this, config);
-        // var node = this;
-        this.on("input", function (msg) {
-            console.log(config);
+        let node = this;
+        node.on("input", function (msg) {
+            //RED.settings.flowFile //可以印出目前這個Flow存放在本機檔案系統上的檔案名稱
+            // console.log(RED.settings.userDir);// /Users/xanxus/.node-red
             msg.payload = msg.payload.toLowerCase();
-            this.send(msg);
+            node.send(msg);
         });
+
     }
     RED.nodes.registerType("lower-case", LowerCaseNode);
 };
