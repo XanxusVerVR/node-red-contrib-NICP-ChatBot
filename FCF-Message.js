2b036f30f8cc64391e19e48e93f0112ca8131485
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2019-02-07 05:54:28 +0800

    初步完成改善Message節點(資料註解未拿掉)

--- FCF-Message.js
+++ FCF-Message.js
@@ -2,12 +2,96 @@ const _ = require("underscore");
 const utils = require("./lib/helpers/utils");
 const MessageTemplate = require("./lib/message-template");
 const emoji = require("node-emoji");
-const colors = require("colors");
+const mustache = require("mustache");
 
 module.exports = function (RED) {
 
+    function extractTokens(tokens, set) {
+        set = set || new Set();
+        tokens.forEach(function (token) {
+            if (token[0] !== 'text') {
+                set.add(token[1]);
+                if (token.length > 4) {
+                    extractTokens(token[4], set);
+                }
+            }
+        });
+        return set;
+    }
+
+    function parseContext(key) {
+        let match = /^(flow|global)(\[(\w+)\])?\.(.+)/.exec(key);
+        if (match) {
+            let parts = {};
+            parts.type = match[1];
+            parts.store = (match[3] === '') ? "default" : match[3];
+            parts.field = match[4];
+            return parts;
+        }
+        return undefined;
+    }
+
+    function NodeContext(msg, nodeContext, parent, escapeStrings, cachedContextTokens) {
+        this.msgContext = new mustache.Context(msg, parent);
+        this.nodeContext = nodeContext;
+        this.escapeStrings = escapeStrings;
+        this.cachedContextTokens = cachedContextTokens;
+    }
+
+    NodeContext.prototype = new mustache.Context();
+
+    NodeContext.prototype.lookup = function (name) {
+        // try message first:
+        try {
+            let value = this.msgContext.lookup(name);
+            if (value !== undefined) {
+                if (this.escapeStrings && typeof value === "string") {
+                    value = value.replace(/\\/g, "\\\\");
+                    value = value.replace(/\n/g, "\\n");
+                    value = value.replace(/\t/g, "\\t");
+                    value = value.replace(/\r/g, "\\r");
+                    value = value.replace(/\f/g, "\\f");
+                    value = value.replace(/[\b]/g, "\\b");
+                }
+                return value;
+            }
+
+            // try flow/global context:
+            let context = parseContext(name);
+            if (context) {
+                let type = context.type;
+                let store = context.store;
+                let field = context.field;
+                let target = this.nodeContext[type];
+                if (target) {
+                    return this.cachedContextTokens[name];
+                }
+            }
+            return '';
+        }
+        catch (err) {
+            throw err;
+        }
+    };
+
+    NodeContext.prototype.push = function push(view) {
+        return new NodeContext(view, this.nodeContext, this.msgContext, undefined, this.cachedContextTokens);
+    };
+
     function Message(config) {
-        console.log(colors.green(config));
+        /*
+        config:
+            { id: '2fc2ff63.9a4f5',
+            type: 'FCF-Message',
+            z: '9fdb8a20.7c04d8',
+            name: '',
+            message: [ { message: '111' }, { message: '222' } ],
+            answer: false,
+            track: false,
+            parse_mode: '',
+            x: 340,
+            y: 620,
+            wires: [ [ 'b43f223e.3261f' ] ] } */
         RED.nodes.createNode(this, config);
         let node = this;
         this.message = config.message;
@@ -15,6 +99,7 @@ module.exports = function (RED) {
         this.parse_mode = config.parse_mode;
         this.transports = ["telegram", "slack", "facebook", "smooch"];
 
+        //此函式會隨機抓一個訊息回傳
         this.pickOne = function (messages) {
             let luck = Math.floor(Math.random() * messages.length);
             return _.isString(messages[luck]) ? messages[luck] : messages[luck].message;
@@ -34,6 +119,7 @@ module.exports = function (RED) {
             let chatId = utils.getChatId(msg);
             let messageId = utils.getMessageId(msg);
             let template = MessageTemplate(msg, node);
+            let is_json = false;
 
             // check transport compatibility
             if (!utils.matchTransport(node, msg)) {
@@ -42,9 +128,9 @@ module.exports = function (RED) {
 
             if (_.isString(node.message) && !_.isEmpty(node.message)) {
                 message = node.message;
-            } else if (_.isArray(node.message) && !this.emptyMessages(node.message)) {
+            } else if (_.isArray(node.message) && !this.emptyMessages(node.message)) {//當有在此節點設置多個訊息時，會進到這個if。只設一個訊息也會進到這if
                 message = node.pickOne(node.message);
-            } else if (_.isString(msg.payload) && !_.isEmpty(msg.payload)) {
+            } else if (_.isString(msg.payload) && !_.isEmpty(msg.payload)) {//當沒有在此節點設置訊息時，會進到這個if
                 message = msg.payload;
             } else if (_.isArray(msg.payload) && !this.emptyMessages(msg.payload)) {
                 message = node.pickOne(msg.payload);
@@ -54,27 +140,80 @@ module.exports = function (RED) {
                 node.error("Empty message");
             }
 
-            msg.payload = {
-                type: "message",
-                content: emoji.emojify(template(message)),
-                chatId: chatId,
-                messageId: messageId,
-                inbound: false
-            };
-            if (msg.whetherToSendLocation) {
-                msg.payload.type = "request";//將type設為request才能在Facebook Out送出詢問位置的請求
-            }
-            msg.payload.options = {};
-            // parse mode
-            if (!_.isEmpty(parse_mode)) {
-                msg.payload.options.parse_mode = parse_mode;
-            }
-            // reply flag
-            if (answer) {
-                msg.payload.options.reply_to_message_id = messageId;
-            }
-            // send out reply
-            node.send(msg);
+            /* tokens:
+                Set { 'flow.aaa', 'global.ccc' }
+                Set { 'msg.payload' }
+            */
+            let promises = [];
+            let tokens = extractTokens(mustache.parse(message));
+            let resolvedTokens = {};
+            tokens.forEach(function (name) {
+                // context:
+                // { type: 'flow', store: undefined, field: 'aaa' }
+                // 當是msg.payload的時候是 undefined
+                let context = parseContext(name);
+                if (context) {
+                    let type = context.type;
+                    let store = context.store;
+                    let field = context.field;
+                    /* target:
+                        {}
+                    */
+                    let target = node.context()[type];
+                    if (target) {
+                        let promise = new Promise((resolve, reject) => {
+                            /*
+                            field:
+                                aaa
+                            store:
+                                undefined
+                            val:
+                                321
+                            */
+                            target.get(field, store, (err, val) => {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    /* resolvedTokens:
+                                        { 'flow.aaa': 321 }
+                                    */
+                                    resolvedTokens[name] = val;
+                                    resolve();
+                                }
+                            });
+                        });
+                        promises.push(promise);
+                        return;
+                    }
+                }
+            });
+            Promise.all(promises).then(function () {
+                message = mustache.render(message, new NodeContext(msg, node.context(), null, is_json, resolvedTokens));
+                // 這時候的message是最終轉換好的字串:This is the payload: 321!
+                msg.payload = {
+                    type: "message",
+                    content: emoji.emojify(template(message)),
+                    chatId: chatId,
+                    messageId: messageId,
+                    inbound: false
+                };
+                if (msg.whetherToSendLocation) {
+                    msg.payload.type = "request";//將type設為request才能在Facebook Out送出詢問位置的請求
+                }
+                msg.payload.options = {};
+                // parse mode
+                if (!_.isEmpty(parse_mode)) {
+                    msg.payload.options.parse_mode = parse_mode;
+                }
+                // reply flag
+                if (answer) {
+                    msg.payload.options.reply_to_message_id = messageId;
+                }
+                // send out reply
+                node.send(msg);
+            }).catch(function (err) {
+                node.error(err.message, msg);
+            });
         });
     }
     RED.nodes.registerType("FCF-Message", Message);
--- lower-case.js
+++ lower-case.js
@@ -9,6 +9,26 @@ module.exports = function (RED) {
         let node = this;
 
         node.on("input", function (msg) {
+            let resolvedTokens = {};
+            let type = "flow";
+            let target = node.context()["global"];
+            console.log(target);
+            let field = "aaa";
+            let name = "flow.aaa";
+            let store = undefined;
+            target.get(field, store, (err, val) => {
+                if (err) {
+                    console.log(err);
+                    // reject(err);
+                } else {
+                    /* resolvedTokens:
+                        { 'flow.aaa': 321 }
+                    */
+                    resolvedTokens[name] = val;
+                    // resolve();
+                }
+            });
+            console.log(resolvedTokens);
             msg.payload = msg.payload.toLowerCase();
             node.send(msg);
         });
