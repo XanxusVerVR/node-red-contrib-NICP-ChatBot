8d275930e66c4ece847cb0f40876225f9cc785f2
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2019-02-19 13:38:05 +0800

    Transformation的刷新Context清單按鈕

--- lower-case.js
+++ lower-case.js
@@ -9,6 +9,11 @@ module.exports = function (RED) {
         node.on("input", function (msg) {
             //RED.settings.flowFile //可以印出目前這個Flow存放在本機檔案系統上的檔案名稱
             // console.log(RED.settings.userDir);// /Users/xanxus/.node-red
+            this.status({
+                fill: "red",
+                shape: "ring",
+                text: "<h1>Hello</h1>"
+            });
             msg.payload = msg.payload.toLowerCase();
             node.send(msg);
         });
