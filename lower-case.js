54601fd3545224b2c51726797d9ac552615da1d4
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2019-03-15 03:06:38 +0800

    可從html中取得檔案系統Context的值並塞入屬性中，並且可以傳到js檔，js可拿到檔案系統Context的資料了!!!

--- lower-case.js
+++ lower-case.js
@@ -3,16 +3,17 @@ module.exports = function (RED) {
         RED.nodes.createNode(this, config);
 
         const flowContext = this.context().flow;//建立並取得context物件
-        let contextFileSystemNodeTypeKey = flowContext.get(config.type, "xanxusContext");
+        // let contextFileSystemNodeTypeKey = flowContext.get(config.type, "xanxusContext");
         // console.log(contextFileSystemNodeTypeKey);
-        this.name2 = contextFileSystemNodeTypeKey[0].name2 || config.name2;//要做單元測試一定要有這行，要把config的值再設到this裡
-        this.datas = contextFileSystemNodeTypeKey[0].datas || config.datas;
-
-
+        // this.name2 = contextFileSystemNodeTypeKey[0].name2 || config.name2;//要做單元測試一定要有這行，要把config的值再設到this裡
+        // this.datas = contextFileSystemNodeTypeKey[0].datas || config.datas;
+        this.name2 = config.name2;
+        this.datas = config.datas;
         const node = this;//要做單元測試一定要有這行，把node參考這裡的this
-        console.log(node);
+        // console.log(node);
         // console.log(flowContext.get(config.type, "xanxusContext"));
         node.on("input", function (msg) {
+            console.log(config);
             // msg.payload = msg.payload.toLowerCase();
             msg.payload = node.name2;
             node.send(msg);
