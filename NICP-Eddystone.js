d5b4d2b5bb7a16e0c231d06b1bf12c5edd31f1cd
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2019-04-22 07:42:10 +0800

    應該要是true

--- NICP-Eddystone.js
+++ NICP-Eddystone.js
@@ -49,6 +49,9 @@ module.exports = function (RED) {
             console.log(node.major);
             console.log(node.minor);
             console.log(node.measuredPower);
+            let boo = !_.isEmpty(node.uuid) && !_.isEmpty(node.major) && !_.isEmpty(node.minor) && !_.isEmpty(node.measuredPower);
+            console.log(`下面應該要是true:`);
+            console.log(boo);
             if (!_.isEmpty(node.uuid) && !_.isEmpty(node.major) && !_.isEmpty(node.minor) && !_.isEmpty(node.measuredPower)) {
                 console.log(`廣播iBeacon`);
                 bleno.startAdvertisingIBeacon(node.uuid, node.major, node.minor, node.measuredPower);
