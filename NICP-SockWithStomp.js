0440c10e1e7b4b466b21d04a1fa57e449f802a1a
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2019-04-25 16:00:45 +0800

    新增NICP-SockWithStomp節點

---
+++ NICP-SockWithStomp.js
@@ -0,0 +1,74 @@
+// 程式碼參考這篇  https://bit.ly/2ZuQtou
+const _ = require("underscore");
+const Stomp = require("stompjs");
+const SockJS = require("sockjs-client");
+let sock;
+let stompClient;
+module.exports = function (RED) {
+    function SockWithStomp(config) {
+
+        RED.nodes.createNode(this, config);
+
+        this.name = config.name || "My SockWithStomp Node";
+        this.sockjsUrl = config.sockjsUrl;
+        this.stompPath = config.stompPath;
+
+        const node = this;
+
+        if (!_.isEmpty(node.sockjsUrl) && !_.isEmpty(node.stompPath)) {
+
+            sock = new SockJS(node.sockjsUrl);
+            stompClient = Stomp.over(sock);
+
+            sock.onopen = function () {
+                console.log("open");
+            };
+
+            sock.onmessage = function (e) {
+                console.log("message", e.data);
+                sock.close();
+            };
+
+            sock.onclose = function () {
+                console.log("close");
+            };
+            let i = 0;
+            const subscribeCallback = function (message) {
+                console.log("received message: " + message);
+                i++;
+                console.log(`印出i:${i}`);
+                const msg = {};
+                msg.payload = JSON.parse(message.body);
+                node.send(msg);
+            };
+
+            const connectCallback = function (frame) {
+                console.log("connect");
+                node.status({
+                    fill: "green",
+                    shape: "dot",
+                    text: "connected"
+                });
+                stompClient.subscribe("/topic/notification/SCS-A", subscribeCallback);
+            };
+            const errorCallback = function (error) {
+                console.log("connect error:");
+                console.log(error);
+                node.status({
+                    fill: "red",
+                    shape: "dot",
+                    text: "disconnect"
+                });
+            };
+            stompClient.connect({}, connectCallback, errorCallback);
+        }
+
+        node.on("close", function (removed, done) {
+            stompClient.disconnect(function () {
+                console.log("disconnect !");
+            });
+            done();
+        });
+    }
+    RED.nodes.registerType("NICP-SockWithStomp", SockWithStomp);
+};
