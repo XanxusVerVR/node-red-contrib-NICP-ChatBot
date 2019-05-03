f1a4bfd7366c2d7fc7679fd6a04510e3cf0e0524
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2019-05-03 12:24:57 +0800

    將SockJS實例獨立出來，寫在SockJS Configuration Credential節點，可Work

--- NICP-SockJS.js
+++ NICP-SockJS.js
@@ -6,10 +6,46 @@ const clc = require("cli-color");
 const green = clc.greenBright;
 const white = clc.white;
 const red = clc.red;
-let sock;
+
 let stompClient;
 module.exports = function (RED) {
-    function SockJS(config) {
+
+    // SockJS Configuration Credential節點
+    function SockJSNode(config) {
+        RED.nodes.createNode(this, config);
+
+        this.sockjsUrl = this.credentials.sockjsUrl;
+        const sock = new _SockJS(this.sockjsUrl);
+        sock.onopen = function () {
+            console.log("open");
+        };
+
+        sock.onmessage = function (e) {
+            console.log("message", e.data);
+            sock.close();
+        };
+
+        sock.onclose = function () {
+            console.log("close");
+        };
+        this.getSockJSInstance = function _getSockJSInstance() {
+            return sock;
+        };
+        this.addition = function _addition(a, b) {
+            return a + b;
+        };
+        // console.log(`印出SockJS Configuration Credential節點的sockjsUrl：${this.sockjsUrl}`);
+    }
+    RED.nodes.registerType("NICP-SockJS Node", SockJSNode, {
+        credentials: {
+            sockjsUrl: {
+                type: "text"
+            }
+        }
+    });
+
+    // SockJS In節點
+    function SockJSIn(config) {
 
         const setStatus = function _setStatus(fill, shape, text) {
             node.status({
@@ -21,29 +57,18 @@ module.exports = function (RED) {
 
         RED.nodes.createNode(this, config);
 
-        this.name = config.name || "My SockJS Node";
-        this.sockjsUrl = config.sockjsUrl;
+        this.name = config.name || "My SockJSIn Node";
         this.destination = config.destination;
+        this.sockJSConfigNode = RED.nodes.getNode(config.sockJSConfigNode);//取得credentials物件
 
         const node = this;
+        // console.log(`1+1是：${node.sockJSConfigNode.addition(1, 1)}`);
 
-        if (!_.isEmpty(node.sockjsUrl) && !_.isEmpty(node.destination)) {
+        if (!_.isEmpty(node.sockJSConfigNode.credentials.sockjsUrl) && !_.isEmpty(node.destination)) {
 
-            sock = new _SockJS(node.sockjsUrl);
-            stompClient = Stomp.over(sock);
+            // sock = new _SockJS(node.sockJSConfigNode.credentials.sockjsUrl);
+            stompClient = Stomp.over(node.sockJSConfigNode.getSockJSInstance());
 
-            sock.onopen = function () {
-                console.log("open");
-            };
-
-            sock.onmessage = function (e) {
-                console.log("message", e.data);
-                sock.close();
-            };
-
-            sock.onclose = function () {
-                console.log("close");
-            };
             const subscribeCallback = function (message) {
                 const msg = {};
                 msg.payload = JSON.parse(message.body);
@@ -82,5 +107,33 @@ module.exports = function (RED) {
             done();
         });
     }
-    RED.nodes.registerType("NICP-SockJS", SockJS);
+    RED.nodes.registerType("NICP-SockJS In", SockJSIn);
+
+    // SockJS Out節點
+    function SockJSOut(config) {
+
+        const setStatus = function _setStatus(fill, shape, text) {
+            node.status({
+                fill: fill,
+                shape: shape,
+                text: text
+            });
+        };
+
+        RED.nodes.createNode(this, config);
+
+        this.name = config.name || "My SockJSOut Node";
+        this.destination = config.destination;
+        this.sockJSConfigNode = RED.nodes.getNode(config.sockJSConfigNode);//取得SockJS Configuration Credential節點的物件
+
+        const node = this;
+
+        // node.on("close", function (removed, done) {
+        //     stompClient.disconnect(function () {
+        //         console.log(yellow("SockWithStomp節點的ID: ") + white(node.id) + yellow(" Stomp連線關閉！"));
+        //     });
+        //     done();
+        // });
+    }
+    RED.nodes.registerType("NICP-SockJS Out", SockJSOut);
 };
