15c592afa3cc6517ea0df40f9e0dc95a77d43d80
XanxusVer.V.R <xanxusver.v.r@gmail.com>
2019-06-12 01:04:07 +0800

    Conditional Trigger現在可動態生成外部設置的設定條件參數了，UI的Bug也修正，目前沒發現奇怪的地方

--- NICP/NICP-ConditionalTrigger.js
+++ NICP/NICP-ConditionalTrigger.js
@@ -3,11 +3,11 @@ module.exports = function (RED) {
     "use strict";
     const RuleManager = require("./lib/RuleManager");
     const ruleManager = null;
-
+    // let ConditionalTriggerContextFileData;
     function andGateNode(n) {
 
         RED.nodes.createNode(this, n);
-
+        const flowContext = this.context().flow;//建立並取得context物件
         const node = this;
 
         this.rules = n.rules || [];
@@ -16,21 +16,52 @@ module.exports = function (RED) {
         this.emitOnlyIfTrue = n.emitOnlyIfTrue || false;
 
         node.status({ fill: "blue", shape: "ring", text: "Loading..." });
-
-        this.ruleManager = new RuleManager(RED, node, this.type);
-        this.ruleManager.storeRule(this.rules).then((result) => {
+        node.ruleManager = new RuleManager(RED, node, node.type);
+        node.ruleManager.storeRule(node.rules).then((result) => {
             if (result) {
                 node.status({ fill: "green", shape: "dot", text: "TRUE" });
             }
             else {
                 node.status({ fill: "red", shape: "dot", text: "FALSE" });
             }
-            if (this.emitOnlyIfTrue && result || !this.emitOnlyIfTrue) {
-                node.send({ topic: this.topic, payload: null, bool: result })
+            if (node.emitOnlyIfTrue && result || !node.emitOnlyIfTrue) {
+                node.send({ topic: node.topic, payload: null, bool: result })
             }
         });
-        node.on('input', function (msg) {
+        let updateToContextFileRules = function _updateToContextFileRules(rules, topic, emitOnlyIfTrue) {
+            node.ruleManager.storeRule(rules).then((result) => {
+                // if (result) {
+                //     node.status({ fill: "green", shape: "dot", text: "TRUE 02" });
+                // }
+                // else {
+                //     node.status({ fill: "red", shape: "dot", text: "FALSE 02" });
+                // }
+                // if (emitOnlyIfTrue && result || !emitOnlyIfTrue) {
+                //     node.send({ topic: topic, payload: null, bool: result })
+                // }
+            });
+        };
 
+
+        node.on('input', function (msg) {
+            flowContext.get(n.type, "xanxusContext", function (err, contextFileSystemNodeTypeKey) {
+                // contextFileSystemNodeTypeKey回傳是陣列，因他可能裝了很多某類型的節點，如裝很多FCF-Sample這節點，但每個陣列元素中的資料都不一樣
+                if (contextFileSystemNodeTypeKey) {
+                    for (let i = 0; i < contextFileSystemNodeTypeKey.length; i++) {
+                        if (contextFileSystemNodeTypeKey[i].name === node.name) {
+                            node.rules = contextFileSystemNodeTypeKey[i].rules;
+                            node.outputTopic = contextFileSystemNodeTypeKey[i].outputTopic;
+                            node.gateType = contextFileSystemNodeTypeKey[i].gateType;
+                            node.emitOnlyIfTrue = contextFileSystemNodeTypeKey[i].emitOnlyIfTrue;
+                            // ConditionalTriggerContextFileData = contextFileSystemNodeTypeKey[i];
+                            updateToContextFileRules(node.rules, node.outputTopic, node.emitOnlyIfTrue);
+                        }
+                    }
+                }
+                else {
+                    console.log("這個Conditional Trigger目前Context File沒有存他的外部設定檔");
+                }
+            });
             this.ruleManager.updateState(msg).then((result) => {
                 if (result) {
                     node.status({ fill: "green", shape: "dot", text: "TRUE" });
@@ -38,12 +69,16 @@ module.exports = function (RED) {
                 else {
                     node.status({ fill: "red", shape: "dot", text: "FALSE" });
                 }
-                if (this.emitOnlyIfTrue && result || !this.emitOnlyIfTrue) {
-                    node.send({ topic: this.topic, payload: msg.payload || null, bool: result });
+                if (node.emitOnlyIfTrue && result || !node.emitOnlyIfTrue) {
+                    node.send({ topic: node.topic, payload: msg.payload || null, bool: result });
                 }
             });
         });
     }
 
     RED.nodes.registerType("NICP-ConditionalTrigger", andGateNode);
+    // RED.httpAdmin.get("/NICP-ConditionalTrigger", RED.auth.needsPermission("NICP-ConditionalTrigger.read"), function (req, res) {
+    //     res.json(ConditionalTriggerContextFileData);
+    // });
+
 };
